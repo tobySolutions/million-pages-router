a98c8f23156227e1b08ec5d67eb9f6689d539af1
tobySolutions <adedejitobiloba7@gmail.com>
2024-04-02 16:56:46 +0100

    chore: add million pages router

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+import million from "million/compiler";
+import MillionCompiler from '@million/lint';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+const millionConfig = {
+  auto: true, // if you're using RSC: auto: { rsc: true },
+};
+
+export default million.next(
+  MillionCompiler.next({})(nextConfig),
+  millionConfig
+);
